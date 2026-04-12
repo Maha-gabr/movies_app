1ca2a7fde2878e152aba608965c7efd8272af4f0
Maha-Gabr <mahagabr523@gmail.com>
2026-04-13 02:14:52 +0300

    Define and implement routes for authentication, movie details, and profile updates

--- lib/core/routes_manager/route_generator.dart
+++ lib/core/routes_manager/route_generator.dart
@@ -2,7 +2,13 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/routes_manager/routes.dart';
 
+import 'package:movies_app/features/main_layout/movie_detail_screen/movie_detail_screen.dart';
+
+import '../../features/auth/ui/forget_pass/forget_pass_screen.dart';
+import '../../features/auth/ui/login/login_screen.dart';
+import '../../features/auth/ui/register/register_screen.dart';
 import '../../features/main_layout/main_layout.dart';
+import '../../features/main_layout/update_profile_screen/update_profile_screen.dart';
 import '../../features/on_boarding/on_boarding.dart';
 
 class RouteGenerator {
@@ -13,6 +19,17 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => const MainLayout());
     case Routes.onBoardingRoute:
       return MaterialPageRoute(builder: (_) => const OnBoarding());
+      case Routes.SignUpRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+      case Routes.signInRoute:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case Routes.forgetPassRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPassScreen());
+      case Routes.detailScreenRoute:
+        final index = settings.arguments as int;
+        return MaterialPageRoute(builder: (_) => MovieDetailScreen(index: index,));
+      case Routes.updateProfileRoute:
+        return MaterialPageRoute(builder: (_) => const UpdateProfileScreen());
 
 
       default:
--- lib/core/routes_manager/routes.dart
+++ lib/core/routes_manager/routes.dart
@@ -1,9 +1,12 @@
 class Routes {
   static const String signInRoute = "/signIn";
-  static const String signUpRoute = "/signUp";
+  static const String SignUpRoute = "/signUp";
   static const String splashRoute = "/splash";
   static const String onBoardingRoute = "/onBoarding";
   static const String mainRoute = "/mainRoute";
+  static const String forgetPassRoute = '/forgetPass';
+  static const String detailScreenRoute ='/MovieDetail';
+  static const String updateProfileRoute ='/updateProfile';
 
 
 
