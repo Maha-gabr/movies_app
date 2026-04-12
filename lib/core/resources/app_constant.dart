214ff3205a78aa55cfd98f807acd163f200fec25
Maha-Gabr <mahagabr523@gmail.com>
2026-04-13 02:45:31 +0300

    Define `AppConstant`

---
+++ lib/core/resources/app_constant.dart
@@ -0,0 +1,7 @@
+import 'app_assets.dart';
+
+class AppConstant {
+  static List<String> avatarList = [AppAssets.avatar1,
+  AppAssets.avatar2,AppAssets.avatar3, AppAssets.avatar1,AppAssets.avatar2,AppAssets.avatar3,];
+
+}
