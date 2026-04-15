9134375f4f1c9297e7813a3a93a96891fa88a9ec
Maha-Gabr <mahagabr523@gmail.com>
2026-04-15 03:07:43 +0300

    Implement search UI and integrate MovieCubit in SearchTap

--- lib/features/main_layout/taps/search/search_tap.dart
+++ lib/features/main_layout/taps/search/search_tap.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/core/resources/app_assets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies_app/features/main_layout/taps/search/widget/custom_text_field.dart';
+
+import '../../../../core/resources/app_color.dart';
+import '../explore/view_model/movie_cubit.dart';
+import '../explore/view_model/movie_state.dart';
+import '../explore/widget/build_filtered_list.dart';
 
 class SearchTap extends StatelessWidget {
   const SearchTap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Image.asset(AppAssets.emptyImage),
+    return Scaffold(
+      backgroundColor: AppColor.black,
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(vertical: 30.h, horizontal: 10.w),
+        child: BlocProvider(
+          create: (context) => MovieCubit(),
+          child: BlocBuilder<MovieCubit, MovieState>(
+            builder: (context, state) {
+              final bloc = context.watch<MovieCubit>();
+              return Column(
+                children: [
+                  SizedBox(
+                      height: 45,
+                      child: customTextField(bloc)
+                  ),
+                  Expanded(
+                    child:
+                    buildFilteredList(bloc),
+                  )
+                ],
+              );
+            },
+          ),
         ),
-      ],
+      ),
     );
   }
 }
