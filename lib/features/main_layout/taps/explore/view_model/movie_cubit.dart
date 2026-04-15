950cc8fc3de6b4df8a288429e31511d52d8b4281
Maha-Gabr <mahagabr523@gmail.com>
2026-04-15 03:08:29 +0300

    Implement MovieCubit and MovieState for genre filtering and movie search functionality

---
+++ lib/features/main_layout/taps/explore/view_model/movie_cubit.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/movie_detail_model/movie_detail_model.dart';
+import '../../../../../movie_detail_model/movie_detail_model_response.dart';
+import 'movie_state.dart';
+class MovieCubit extends Cubit<MovieState> {
+  MovieCubit()
+      : super(MovieState(
+    selectedGenre: 'All',
+    filteredMovies: MovieDetailModelResponse.movieDetail,
+  ),
+  ){
+    applyFilter();
+  }
+  /// All unique genres extracted from movie data, "All" always first
+  List<String> get allGenres {
+    return [
+      'All',
+      ...{
+        ...MovieDetailModelResponse.movieDetail.expand((m) => m.genres),
+      }.toList()..sort(),
+    ];
+  }
+int selectedIndex =0;
+  void changeIndex(int index) {
+    selectedIndex = index;
+    applyFilter();
+    emit(state.copyWith(
+      selectedGenre: allGenres[index],
+    ));
+  }
+  List<MovieDetailModel> filterList = [];
+  void applyFilter() {
+    final selectedGenre = allGenres[selectedIndex];
+    if (selectedIndex == 0) {
+      filterList = MovieDetailModelResponse.movieDetail;
+    } else {
+      filterList = MovieDetailModelResponse.movieDetail
+          .where((movie) => movie.genres.contains(selectedGenre))
+          .toList();
+    }
+    emit(state.copyWith(
+      filteredMovies: filterList,
+    ));
+  }
+
+  void searchByMovieName(String query) {
+    final text = query.trim().toLowerCase();
+
+     filterList = text.isEmpty
+        ? MovieDetailModelResponse.movieDetail
+        : MovieDetailModelResponse.movieDetail.where((movie) {
+      return movie.title.toLowerCase().contains(text);
+    }).toList();
+
+    emit(state.copyWith(
+      filteredMovies: filterList,
+    ));
+  }
+
+
+}
---
+++ lib/features/main_layout/taps/explore/view_model/movie_state.dart
@@ -0,0 +1,21 @@
+import '../../../../../movie_detail_model/movie_detail_model.dart';
+
+class MovieState {
+  final String selectedGenre;
+  final List<MovieDetailModel> filteredMovies;
+
+  const MovieState({
+    required this.selectedGenre,
+    required this.filteredMovies,
+  });
+
+  MovieState copyWith({
+    String? selectedGenre,
+    List<MovieDetailModel>? filteredMovies,
+  }) {
+    return MovieState(
+      selectedGenre: selectedGenre ?? this.selectedGenre,
+      filteredMovies: filteredMovies ?? this.filteredMovies,
+    );
+  }
+}
