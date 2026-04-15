1fbaa25c03a379245e6df7c3bd964a332e90d845
Maha-Gabr <mahagabr523@gmail.com>
2026-04-15 03:09:06 +0300

    Implement Explore tab UI with genre filtering and BLoC integration

--- lib/features/main_layout/taps/explore/explore_tap.dart
+++ lib/features/main_layout/taps/explore/explore_tap.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
-class ExploreTap extends StatelessWidget {
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies_app/core/resources/app_color.dart';
+import 'package:movies_app/features/main_layout/taps/explore/view_model/movie_cubit.dart';
+import 'package:movies_app/features/main_layout/taps/explore/view_model/movie_state.dart';
+import 'package:movies_app/features/main_layout/taps/explore/widget/build_tap_bar.dart';
+import 'package:movies_app/features/main_layout/taps/explore/widget/build_filtered_list.dart';
+class ExploreTap extends StatefulWidget {
   const ExploreTap({super.key});
+  @override
+  State<ExploreTap> createState() => _ExploreTapState();
+}
+class _ExploreTapState extends State<ExploreTap>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
 
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColor.black,
+      body: BlocProvider(
+        create: (context) => MovieCubit(),
+        child: BlocBuilder<MovieCubit, MovieState>(
+          builder: (context, state) {
+            final browseCubit = context.watch<MovieCubit>();
+            return Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 5.w,vertical: 25.h),
+              child: Column(
+                children: [
+                  DefaultTabController(
+                    length: browseCubit.allGenres.length,
+                    child: buildTapBar(browseCubit),
+                  ),
+                 Expanded(
+                     child:
+                     buildFilteredList(browseCubit)
+                 )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
-
