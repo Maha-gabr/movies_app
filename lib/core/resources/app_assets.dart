11ec67d0675d93ff0377dff2cc1babdacd0ba200
Maha-Gabr <mahagabr523@gmail.com>
2026-04-16 00:55:37 +0300

    Update app assets with new logos, icons, and image resources

--- lib/core/resources/app_assets.dart
+++ lib/core/resources/app_assets.dart
@@ -1,4 +1,5 @@
 class AppAssets {
+  static const String mainLogo = 'assets/images/images/main_logo.png';
   static const List<String> onBoardingList=[
     'assets/images/on_boarding/On_boarding1.png',
     'assets/images/on_boarding/On_boarding2.png',
@@ -7,4 +8,36 @@ class AppAssets {
     'assets/images/on_boarding/On_boarding5.png',
     'assets/images/on_boarding/On_boarding6.png'
   ];
+  static const String avatar1 = 'assets/images/images/avatar1.png';
+  static const String avatar2 = 'assets/images/images/avatar2.png';
+  static const String avatar3 = 'assets/images/images/avatar3.png';
+  static const String googleIcon ='assets/svg_icons/google_icon.svg';
+  static const String forgetPassIcon ='assets/images/images/Forgot password_image.png';
+  static const String homeBg ='assets/images/images/home_bg.png';
+  //
+  // static const String movieImage1 ='assets/images/movies_images/movie1.png';
+  // static const String movieImage2 ='assets/images/movies_images/movie2.png';
+  // static const String movieImage3 ='assets/images/movies_images/movie3.png';
+  // static const String movieImage4 ='assets/images/movies_images/movie4.png';
+  // static const String movieImage5 ='assets/images/movies_images/movie5.png';
+  // static const String movieImage6 ='assets/images/movies_images/movie6.png';
+  // static const String movieImage7 ='assets/images/movies_images/movie7.png';
+  // static const String movieImage8 ='assets/images/movies_images/movie8.png';
+  // static const String movieImage9 ='assets/images/movies_images/movie9.png';
+  // static const String movieImage10 ='assets/images/movies_images/movie10.png';
+  // static const String movieImage11 ='assets/images/movies_images/movie11.png';
+  // static const String movieImage12 ='assets/images/movies_images/movie12.png';
+  // static const String movieImage13 ='assets/images/movies_images/movie13.png';
+  // static const String movieImage14 ='assets/images/movies_images/movie14.png';
+  // static const String movieImage15 ='assets/images/movies_images/movie15.png';
+  // static const String movieImage16 ='assets/images/movies_images/movie16.png';
+  //
+
+  static const String emptyImage ='assets/images/images/Empty.png';
+  static const String playIcon ='assets/svg_icons/play_icon.svg';
+  static const String noProfile ='assets/images/images/no_profile.png';
+
+
+
+
 }
